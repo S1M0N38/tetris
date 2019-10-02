ab0455d76980c748f6ead71ea8b481a9f6b5fa81
S1M0N38 <bertolottosimone@gmail.com>
2019-10-02 12:47:00 +0200

    Hadling level progression and FPS

--- src/main.cpp
+++ src/main.cpp
@@ -20,33 +20,16 @@ int main(int argc, char const *argv[]) {
     halfdelay(1);
 
     while (true) {
-        input = getch();
-        clear();
-        game.trasformTetromino(input);
-        game.render();
-        refresh();
-        std::this_thread::sleep_for(std::chrono::milliseconds(33));
+        for (int i = 0; game.level < 10 && i < 10 - game.level; ++i) {
+            input = getch();
+            game.trasformTetromino(input);
+            game.render();
+            refresh();
+        }
+        game.updateState();
     }
 
     endwin();
 
 
-    /*
-
-    Game game;
-
-    while (true) {
-        for (int i = 0; i < 10; i++) {
-            char input = getch();
-
-            if (input == '↑') Game.tetromino.rotate();
-            else if (input == '⟵' || input = '→' || input == '↓') Game.tetromino.move(input)
-            else pass // expected ERR or other char
-
-            Game.render();
-            std::this_thread::sleep_for(std::chrono::milliseconds(33));
-        }
-        Game.updateBoard();
-    }
-    */
 }
